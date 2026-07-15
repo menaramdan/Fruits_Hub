60319748c0b73eaff24b65db2034794ec9d293af
Mena <menar5693@gmail.com>
2026-07-15 13:28:11 +0300

    add lottie package

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppIcon
+  /// assets/images/App_Icon.png
+  static const String assetsImagesAppIcon = "assets/images/App_Icon.png";
+}
+
